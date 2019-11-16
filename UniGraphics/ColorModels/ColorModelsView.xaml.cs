384eb454865d256a96127c1a9ed701210d1839b5
Bilas Yurii <bilas.yurii@gmail.com>
2019-11-16 13:25:04 +0200

    Moved ColorModels to separate folder.

---
+++ UniGraphics/ColorModels/ColorModelsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace UniGraphics.ColorModels
+{
+    /// <summary>
+    /// Interaction logic for ColorModelsView.xaml
+    /// </summary>
+    public partial class ColorModelsView : Page
+    {
+        public ColorModelsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
