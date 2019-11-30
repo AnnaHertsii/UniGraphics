39fed7513deb5b8104f09885c82c7ba3382c89f7
Anna <annahertsiy@gmail.com>
2019-11-30 22:30:52 +0100

    Pause button

--- UniGraphics/Transformation/TransformationView.xaml.cs
+++ UniGraphics/Transformation/TransformationView.xaml.cs
@@ -24,5 +24,34 @@ namespace UniGraphics.Transformation
         {
             InitializeComponent();
         }
+
+        private void StudyMaterialsClicked(object sender, RoutedEventArgs e)
+        {
+            StudyWindow studyWindow = new StudyWindow();
+            studyWindow.ShowDialog();
+        }
+
+        private void HelpClicked(object sender, RoutedEventArgs e)
+        {
+            HelpWindow helpWindow = new HelpWindow();
+            helpWindow.ShowDialog();
+        }
+
+        private void CreditsClicked(object sender, RoutedEventArgs e)
+        {
+            CreditsWindow creditWindow = new CreditsWindow();
+            creditWindow.ShowDialog();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartButton.Visibility = Visibility.Hidden;
+            PauseButton.Visibility = Visibility.Visible;
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
